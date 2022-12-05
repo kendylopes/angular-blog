2e971f3a2756df26dd60e8e7d18e527d6e718c29
Kennedy Lopes <kendylopes@gmail.com>
2022-12-05 14:08:04 -0400

    Content create

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Mavel studio e os novos vingadores...",
+    "description": "Preste a ser lançado mais um emocionante filme da franquia Mavel",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH9wuoSx9iqLzRg2onJmovGOxhl3zXbTbQXA&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title": "Nova serie anunciada, terá estreia prorrogada para o proximo semestre",
+    "description": "cinemas com novo Vingador",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCZHdu0cfEKl9jl6z7UpB34MwFu2-jQDI2hg&usqp=CAU"
+  }
+]
